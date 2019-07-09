d511aef5a67276479287234362acb07f68e0e261
Ayush James <ayush.james1996@gmail.com>
2019-07-10 06:48:36 +1000

    Game Logic initialised

--- js/gamelogic.js
+++ js/gamelogic.js
@@ -1,49 +1,65 @@
-// console.log("hello");
-//
-//
-// player 1 = key1
-// player 2 = key2
-const playerSymbol = ["🐶", "🐱"];
-let playerTurn = playerSymbol[0];
-
-// 1. C
-
-
-//
-// On
-// player 1's turn = on click, image should be key1
-// player 2's turn = on click, image should be key2
-//
-//
-// When Player x gets three in row.
-// Player x is the winner!
-//
-// When there is a draw.
-// It is a draw!
-
 //need to only check this when three moves have been made so that the final "else" wont show after just two moves!? I guess you can call on this after player1's turn the third time.
 
 //Make it also that the winning three is highlighted!
 
-const gameOutcomes = function () {
- if ("#a2") {
-   console.log('Winner');
- }
-};
+// const player1 = ["a1", "a4", 'a6'];
 
-
-
-  // if ($("#a1") === $("#a2") === $("#a3") || $("#b1") === $("#b2") === $("#b3") || $("#c1") === $("#c2") === $("#c3")) {
-  if() {
-    console.log(`hello`);
-  }
-};
-//     console.log(`Player [i] is the Winner!`); //potenital win sequence 1 // almost mak
-//   } else if ($("#a1") === $("#b1") === $("#c1") || $("#a2") === $("#b2") === $("#c2") || $("#a3") === $("#b3") === $("#c3")) {
-//     console.log(`Player [i] is the Winner!`); //potenital win sequence 2
-//   } else if ( $("#a1"[0].innerText) === $("#b2"[0].innerText) === $("#c3"[0].innerText) || $("#a3"[0].innerText) === $("#b2"[0].innerText) === $("#c1"[0].innerText)) {
-//     console.log(`Player [i] is the Winner!`); //potenital win sequence 3
-//   } else () {
-//     console.log(`The game is a Draw.`);
-//   };
+// const gameOutcomes = function (userSelected) {
+//   const winner = {
+//     winnerPath1: ["a1","a4","a6"], [],[],[],[]];
+//     winnerPath2
+//   // userSelected is array ["a3", "a4", "a2"];
+// for (var i = 0; i < userSelected.length; i++) {
+//     let select =userSelected[i];
+//     if (winnerP.includes(select)){
+//       //found
+//     }
 // }
+
+let player1Array = [];
+let player2Array = [];
+//playerClicked = 0;
+
+// create a click event on the common class for all boxes
+let clickEvent = $(".cell").click(function() {
+  if ((player1Array.length - player2Array.length) === 0 ) {
+    player1Array.push($(this).attr("id"));
+    //AJ: have to make sure that the push isn't done twice into the array. so that one array contains A1 and A1 twice for eg. 
+    alert("🐶");
+  } else {
+    player2Array.push($(this).attr("id"));
+    alert("🐱");
+  }
+});
+
+// inside the event have if else {
+// check playerClicked is even - if yes then
+// push this.id into the player1Arr
+//player1Array.push($(this).attr("id"));
+// display X
+// else
+// push this.id into the player2Arr
+// display O
+
+// playerClicked++
+
+// player1 array.length === 5 and player 2 array .length ===4 {
+// gameOutcomes(player1Array)
+//}
+
+
+
+const gameOutcomes = function (playerArray) {
+ if ( (playerArray.includes("a1") && playerArray.includes("a2") && playerArray.includes("a3")) ||
+      (playerArray.includes("b1") && playerArray.includes("b2") && playerArray.includes("b3")) ||
+      (playerArray.includes("c1") && playerArray.includes("c2") && playerArray.includes("c3")) ||
+      (playerArray.includes("a1") && playerArray.includes("b1") && playerArray.includes("c1")) ||
+      (playerArray.includes("a2") && playerArray.includes("b2") && playerArray.includes("c2")) ||
+      (playerArray.includes("a3") && playerArray.includes("b3") && playerArray.includes("c3")) ||
+      (playerArray.includes("a1") && playerArray.includes("b2") && playerArray.includes("c3")) ||
+      (playerArray.includes("a3") && playerArray.includes("b2") && playerArray.includes("c1"))) {
+
+      console.log("winner");
+
+      }
+};
