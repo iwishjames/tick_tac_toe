44b06f8683110820d403d267e07752ceef523014
Ayush James <ayush.james1996@gmail.com>
2019-07-09 15:11:53 +1000

    first commit

---
+++ js/gamelogic.js
@@ -0,0 +1,49 @@
+// console.log("hello");
+//
+//
+// player 1 = key1
+// player 2 = key2
+const playerSymbol = ["🐶", "🐱"];
+let playerTurn = playerSymbol[0];
+
+// 1. C
+
+
+//
+// On
+// player 1's turn = on click, image should be key1
+// player 2's turn = on click, image should be key2
+//
+//
+// When Player x gets three in row.
+// Player x is the winner!
+//
+// When there is a draw.
+// It is a draw!
+
+//need to only check this when three moves have been made so that the final "else" wont show after just two moves!? I guess you can call on this after player1's turn the third time.
+
+//Make it also that the winning three is highlighted!
+
+const gameOutcomes = function () {
+ if ("#a2") {
+   console.log('Winner');
+ }
+};
+
+
+
+  // if ($("#a1") === $("#a2") === $("#a3") || $("#b1") === $("#b2") === $("#b3") || $("#c1") === $("#c2") === $("#c3")) {
+  if() {
+    console.log(`hello`);
+  }
+};
+//     console.log(`Player [i] is the Winner!`); //potenital win sequence 1 // almost mak
+//   } else if ($("#a1") === $("#b1") === $("#c1") || $("#a2") === $("#b2") === $("#c2") || $("#a3") === $("#b3") === $("#c3")) {
+//     console.log(`Player [i] is the Winner!`); //potenital win sequence 2
+//   } else if ( $("#a1"[0].innerText) === $("#b2"[0].innerText) === $("#c3"[0].innerText) || $("#a3"[0].innerText) === $("#b2"[0].innerText) === $("#c1"[0].innerText)) {
+//     console.log(`Player [i] is the Winner!`); //potenital win sequence 3
+//   } else () {
+//     console.log(`The game is a Draw.`);
+//   };
+// }
