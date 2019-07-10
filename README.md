bce7d3ab3eacbf2ad1cf5ef89d6a6990f01e6af6
Ayush James <ayush.james1996@gmail.com>
2019-07-10 14:58:00 +1000

    added README

---
+++ README.md
@@ -0,0 +1,19 @@
+# Project 0: Tick Tac Toe
+
+This is a simple game
+
+## features
+
+it has lasers
+
+## bugs
+
+## TODO
+
+## technologies.
+- Made with jquery
+- also uses funtext to make
+
+
+## Thank you
+-  
--- js/gamelogic.js
+++ js/gamelogic.js
@@ -2,22 +2,44 @@
 
 //Make it also that the winning three is highlighted!
 
-// const player1 = ["a1", "a4", 'a6'];
-
-// const gameOutcomes = function (userSelected) {
-//   const winner = {
-//     winnerPath1: ["a1","a4","a6"], [],[],[],[]];
-//     winnerPath2
-//   // userSelected is array ["a3", "a4", "a2"];
-// for (var i = 0; i < userSelected.length; i++) {
-//     let select =userSelected[i];
-//     if (winnerP.includes(select)){
-//       //found
-//     }
-// }
+// const gameOutcomes = function () {
+//   winner = false;
+//  if ( (playerArray.includes("a1") && playerArray.includes("a2") && playerArray.includes("a3")) ||
+//       (playerArray.includes("b1") && playerArray.includes("b2") && playerArray.includes("b3")) ||
+//       (playerArray.includes("c1") && playerArray.includes("c2") && playerArray.includes("c3")) ||
+//       (playerArray.includes("a1") && playerArray.includes("b1") && playerArray.includes("c1")) ||
+//       (playerArray.includes("a2") && playerArray.includes("b2") && playerArray.includes("c2")) ||
+//       (playerArray.includes("a3") && playerArray.includes("b3") && playerArray.includes("c3")) ||
+//       (playerArray.includes("a1") && playerArray.includes("b2") && playerArray.includes("c3")) ||
+//       (playerArray.includes("a3") && playerArray.includes("b2") && playerArray.includes("c1"))) {
+//
+//       return winner = true; //return True // have ti return something else here
+//
+//       }
+// };
 
 let player1Array = [];
 let player2Array = [];
+
+
+// const gameWinner = function () {
+//   if (playRegister.length >= 5 && playRegister.length <= 9) {
+//     push player1 array into gameOutcomes //this goes first because Player 1 always has the first chance to win.
+//     if (outcome = true) {
+//       $(".cell").off("click");
+//       display button to play again, which resets the game.
+//
+//     } else {
+//         pop player1 array from gameOutcomes
+//         push player2 array into gameOutcomes{
+//         if (outcome = true) {
+//           $(".cell").off("click");
+//           display button to play again, which resets the game.
+//         } else {pop player2 array from gameOutcomes}
+// }
+// });
+
+// let playRegister = []; //used to log all the plays made and the length of the plays.
 
 // create a click event on the common class for all boxes
 // inside the event have if else {
@@ -34,36 +56,53 @@ let player2Array = [];
 // player1 array.length === 5 and player 2 array .length ===4 {
 // gameOutcomes(player1Array)
 //}
+
 $(".playerTurn").text("Player 1's turn."); //As player1 always goes first.
 
 $(".cell").click(function() {
+    playRegister.push($(this).attr("id"));
     if ((player1Array.length - player2Array.length) === 0 ) { //figures out of the turn is even or odd.
         player1Array.push($(this).attr("id"));
-        //AJ: have to make sure that the push isn't done twice into the array. so that one array contains A1 and A1 twice for eg.
         $(this).text("🐶");
         $(this).off("click");
-        $(".playerTurn").text("Player 2's turn.");
+        // if (gameOutcomes(player1Array) == true) {
+        //     $(".winnerMessage").text("Player 1 is the Winner!")
+        // } else {
+            $(".playerTurn").text("Player 2's turn.");
+        };
     } else {
         player2Array.push($(this).attr("id"));
         $(this).text("🐱");
         $(this).off("click");
-        $(".playerTurn").text("Player 1's turn.");
-    }
-});
+        gameOutcomes(player2Array);
+        // if (gameOutcomes(player2Array) == true) {
+        //    $(".winnerMessage").text("Player 2 is the Winner!")
+        } else {
+          $(".playerTurn").text("Player 1's turn.");
+        };
+    };
 
 
+// OPTIMISATION for below WOULD BE TO JUST CALL the gameOutcomes on WHO EVER's TURN IT IS.
+/////////////////////////
 
-const gameOutcomes = function (playerArray) {
- if ( (playerArray.includes("a1") && playerArray.includes("a2") && playerArray.includes("a3")) ||
-      (playerArray.includes("b1") && playerArray.includes("b2") && playerArray.includes("b3")) ||
-      (playerArray.includes("c1") && playerArray.includes("c2") && playerArray.includes("c3")) ||
-      (playerArray.includes("a1") && playerArray.includes("b1") && playerArray.includes("c1")) ||
-      (playerArray.includes("a2") && playerArray.includes("b2") && playerArray.includes("c2")) ||
-      (playerArray.includes("a3") && playerArray.includes("b3") && playerArray.includes("c3")) ||
-      (playerArray.includes("a1") && playerArray.includes("b2") && playerArray.includes("c3")) ||
-      (playerArray.includes("a3") && playerArray.includes("b2") && playerArray.includes("c1"))) {
+// //
+// if (playRegister.length >= 5 && playRegister.length <= 9) {
+//     push player1 array into gameOutcomes //this goes first because Player 1 always has the first chance to win.
+//     if (outcome = true) {
+//       $(".winnerMessage").text("Player 1 is the Winner!");
+//       $(".cell").off("click");
+//       display button to play again, which resets the game.
+//
+//     } else {
+//         pop player1 array from gameOutcomes
+//         push player2 array into gameOutcomes{
+//         if (outcome = true) {
+//           $(".winnerMessage").text("Player 2 is the Winner!");
+//           $(".cell").off("click");
+//           display button to play again, which resets the game.
+//         } else {pop player2 array from gameOutcomes}
+// }
+// });
 
-      console.log("winner");
-
-      }
-};
+///////////////////////////
